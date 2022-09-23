ef32d2078da5c9cbfe6a2625f0751abaa96c0260
pnrbrk41 <113897255+pnrbrk41@users.noreply.github.com>
2022-09-23 22:38:11 +0300

    son
    sonswitch

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+    
+
+	namespace switchh
+	{
+
+
+   class Program
+   
+   {
+      static void Main (string[] args )
+      {
+          int mounth = DateTime.Now.Month;
+          
+
+          
+        switch (mounth)
+        {
+           case 1:
+           Console.WriteLine("Ocak ayındasınız.");
+           break;
+           case 2:
+           Console.WriteLine("Şubat ayındasınız.");
+           break;
+           case 3:
+           Console.WriteLine("Mart ayındaısnız.");
+           break;
+          
+           default:
+             Console.WriteLine("yanlış veri girdiniz.");
+             break;
+        }
+        
+
+        switch (mounth)
+        {
+            case 12:
+            case 1:
+            case 2:
+              Console.WriteLine("Kış ayındasınız.");
+              break;
+              default:
+              break;
+
+
+
+
+
+        }
+      }
+   }
+    }
